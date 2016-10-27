672a33877d9f8863d4f4dd23c8107f18b29de0b3
harikavemula91 <harika.vemula11@gmail.com>
2016-10-27 19:20:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/blog', function (req, res){
 });
 
 app.get('/blog2', function (req, res){
-    res.send(' fashion ');
+    res.sendFile(path.join(__dirname, 'ui', 'blog2.html'));
 });
 
 app.get('/blog3', function (req, res){

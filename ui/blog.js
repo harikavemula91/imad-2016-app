844663603cdf6e837adab7b3b75949e41f4e3c7d
harikavemula91 <harika.vemula11@gmail.com>
2016-10-27 22:49:18 +0530

    [imad-console] Updates ui/blog.js

--- ui/blog.js
+++ ui/blog.js
@@ -1,1 +1,10 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'diwali-115a.jpg'));
+});
